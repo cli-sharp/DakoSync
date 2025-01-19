bd756cfebc032288e09c6c46b8eabdbf5bd5626e
Christian Linse <christian.linse@gmail.com>
2025-01-19 16:32:36 +0100

    dev

---
+++ Controllers/WfsObjectFeatureBookingController.cs
@@ -0,0 +1,34 @@
+﻿namespace DakoSync.Controllers;
+
+using DakoSync.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class WfsObjectFeatureBookingController : Controller
+{
+    [HttpPost("accounts/{accountUid}/wfsObjects/{ObjectUid}/featureBooking")]
+    public ActionResult<WfsObjectFeatureBooking> CreateWfsObjectFeatureBooking(
+        string accountUid, string objectUid, [FromBody] WfsObjectFeatureBooking wfsObject)
+    {
+        return Created();
+    }
+
+    [HttpGet("accounts/{accountUid}/wfsObjects/{ObjectUid}/featureBooking")]
+    public ActionResult<WfsObjectFeatureBooking> ReadWfsObjectFeatureBooking(
+        string accountUid, string objectUid)
+    {
+        return Ok();
+    }
+
+    [HttpPut("accounts/{accountUid}/wfsObjects/{ObjectUid}/featureBooking")]
+    public ActionResult<WfsObjectFeatureBooking> UpdateWfsObjectFeatureBooking(
+        string accountUid, string objectUid, [FromBody] WfsObjectFeatureBooking wfsObject)
+    {
+        return Created();
+    }
+
+    [HttpDelete("accounts/{accountUid}/wfsObjects/{ObjectUid}/featureBooking")]
+     public ActionResult DeleteWfsObjectFeatureBooking(string accountUid, string objectUid)
+    {
+        return NoContent();
+    }
+}
