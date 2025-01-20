35884edc64ce0151d7eb2b1a70e780f3c3d1fc80
Christian Linse <christian.linse@gmail.com>
2025-01-20 11:31:15 +0100

    dev

--- Program.cs
+++ Program.cs
@@ -24,21 +24,44 @@ public class OperationFilter : IOperationFilter
     public void Apply(OpenApiOperation operation, OperationFilterContext context)
     {
         operation.Responses.Add("401", new OpenApiResponse() { Description = "Unauthorized" });
-        operation.Responses.Add("500", new OpenApiResponse() { Description = "Internal server error", Content = new Dictionary<string, OpenApiMediaType>()
+        operation.Responses.Add(
+            "500",
+            new OpenApiResponse()
+            {
+                Description = "Internal server error",
+                Content = new Dictionary<string, OpenApiMediaType>()
                 {
                     ["application/json"] = new OpenApiMediaType
                     {
                         Schema = context.SchemaGenerator.GenerateSchema(typeof(string), context.SchemaRepository),
                     },
-                }});
+                },
+            });
 
         var method = context.MethodInfo.GetCustomAttributes(true).OfType<HttpMethodAttribute>().Single();
-        if (method is HttpDeleteAttribute)
+
+        if (method is HttpPostAttribute || method is HttpPutAttribute)
         {
-            operation.Responses.Add("404", new OpenApiResponse()
-            {
-                Description = "UID not found",
-            });
+            operation.Responses.Add(
+                "400",
+                new OpenApiResponse()
+                {
+                    Description = "Model invalid",
+                    Content = new Dictionary<string, OpenApiMediaType>()
+                    {
+                        ["application/json"] = new OpenApiMediaType
+                        {
+                            Schema = context.SchemaGenerator.GenerateSchema(typeof(string), context.SchemaRepository),
+                        },
+                    },
+                });
+
+            operation.Responses.Add("409", new OpenApiResponse() { Description = "UID already exists" });
+        }
+
+        if (method is HttpGetAttribute || method is HttpPutAttribute || method is HttpDeleteAttribute)
+        {
+            operation.Responses.Add("404", new OpenApiResponse() { Description = "UID not found" });
         }
     }
 }
